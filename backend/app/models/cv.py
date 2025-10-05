4c471a79b84c279d9e014057fc20869904dee112
imran <imraniqbal829@gmail.com>
2025-10-05 20:15:47 +0200

    add models CV

---
+++ backend/app/models/cv.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
+from .database import Base
+
+class CV(Base):
+    __tablename__ = "cvs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, index=True)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
